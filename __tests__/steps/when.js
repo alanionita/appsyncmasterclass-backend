68fe5ced5b0f96fe8b1c047af29fa655da36c767
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-03-10 16:20:20 +0000

    FEAT (tests/steps/when) : adds step for user_signs_up() implementing Cognito Client
    Adds custom logic to sign up a user with Cognito
    
    Adds custom logic to confirm a user signup with Cognito
    
    Adds @aws-sdk/client-cognito-identity-provider module
    
    Adds package.json script to run e2e tests

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,3 +1,5 @@
+const { AdminConfirmSignUpCommand, CognitoIdentityProviderClient, SignUpCommand } = require("@aws-sdk/client-cognito-identity-provider"); // ES Modules import
+
 require('dotenv').config()
 
 async function invoke_confirmUserSignup(username, name, email) {
@@ -36,6 +38,63 @@ async function invoke_confirmUserSignup(username, name, email) {
 
 }
 
+async function user_signs_up(password, name, email) {
+    try {
+        const cognito = new CognitoIdentityProviderClient({});
+
+        const userPoolId = process.env.COGNITO_USER_POOL_ID
+        const clientId = process.env.COGNITO_CLIENT_ID
+
+        if (!userPoolId) {
+            throw Error("Missing Environment vars: COGNITO_USER_POOL_ID")
+        }
+
+        if (!clientId) {
+            throw Error("Missing Environment vars: COGNITO_CLIENT_ID")
+        }
+
+        const input = {
+            ClientId: clientId, // required
+            Username: email, // required
+            Password: password,
+            UserAttributes: [ // AttributeListType
+                { // AttributeType
+                    Name: "name", // required
+                    Value: name,
+                },
+            ],
+        }
+
+        const command = new SignUpCommand(input);
+        const response = await cognito.send(command);
+        if (response) {
+            const username = response.UserSub;
+            console.info(`User has signed up - [${email}]`)
+            
+            const confirmCommand = new AdminConfirmSignUpCommand({
+                UserPoolId: userPoolId, // required
+                Username: username
+            })
+
+            await cognito.send(confirmCommand)
+
+            console.info(`Confirmed signup - [${email}]`)
+            
+            return {
+                username,
+                name, 
+                email
+            }
+        }
+
+    } catch (err) {
+        console.error('Err [tests/steps/user_signs_up] : ', err.message);
+        console.info(JSON.stringify(err.stack))
+    }
+
+}
+
 module.exports = {
-    invoke_confirmUserSignup
+    invoke_confirmUserSignup,
+    user_signs_up
 }
