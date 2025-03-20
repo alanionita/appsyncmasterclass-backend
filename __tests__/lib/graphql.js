ef35c21bc23c87c2f61016f5543da5320bba7534
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-03-20 12:39:19 +0000

    REFACTPR (tests/lib): adds new graphql library

--- __tests__/lib/graphql.js
+++ __tests__/lib/graphql.js
@@ -1,43 +1,63 @@
 const http = require('axios')
 
-function throwOnErrors({ query, variables, errors }) {
-    if (errors) {
-        const message = `
-            query: ${query.substr(0, 100)}
-            variables: ${JSON.stringify(variables, null, 2)}
-            errors: ${JSON.stringify(errors, null, 2)}
-        `
+const fragments = {}
+const registerFragment = (name, fragment) => fragments[name] = fragment
 
-        throw new Error(message)
+const throwOnErrors = ({ query, variables, errors }) => {
+    if (errors) {
+        const errorMessage = `
+            query: ${query.substr(0, 100)}
+            variales: ${JSON.stringify(variables, null, 2)}
+            error: ${JSON.stringify(errors, null, 2)}
+        `
+        throw new Error(errorMessage)
     }
 }
 
-module.exports = async ({ url, query, variables = {}, auth }) => {
-    try {
-        const headers = {}
+function* findUsedFragments(query, usedFragments = new Set()) {
+    for (const name of Object.keys(fragments)) {
+        if (query.includes(name) && !usedFragments.has(name)) {
+            usedFragments.add(name)
+            yield name
 
-        if (auth) {
-            headers.Authorization = auth
+            const fragment = fragments[name]
+            const nestedFragments = findUsedFragments(fragment, usedFragments)
+
+            for (const nestedName of Array.from(nestedFragments)) {
+                yield nestedName
+            }
         }
+    }
+}
 
+module.exports.registerFragment = registerFragment
+module.exports.GraphQL = async ({url, query, variables = {}, auth}) => {
+    const headers = {}
+    if (auth) {
+        headers.Authorization = auth
+    }
+
+    const usedFragments = Array
+        .from(findUsedFragments(query))
+        .map(name => fragments[name])
+
+    try {
         const resp = await http({
             method: 'post',
             url,
             headers,
             data: {
-                query,
+                query: [query, ...usedFragments].join('\n'),
                 variables: JSON.stringify(variables)
             }
         })
 
-        const { data, errors } = resp.data;
-
+        const { data, errors } = resp.data
         throwOnErrors({ query, variables, errors })
-
-        return data;
-    } catch (err) {
-        const errors = err.response && err.response.data && err.response.data.errors
+        return data
+    } catch (caught) {
+        const errors = caught.response && caught.response.data && caught.response.data.errors
         throwOnErrors({ query, variables, errors })
-        throw err;
+        throw caught
     }
 }
