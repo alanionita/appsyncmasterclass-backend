0ca55196635e85687799413b521f9e8b1e29cd45
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-05-02 16:24:49 +0100

    FEAT (tests/integration): adds second test scenario for reply
    Handling retweets, which leverage a recursive call to the original tweet to get all the authors

--- __tests__/cases/integration/reply.test.js
+++ __tests__/cases/integration/reply.test.js
@@ -3,6 +3,7 @@ const given = require("../../steps/given");
 const when = require("../../steps/when");
 const then = require("../../steps/then");
 const { waitSec } = require("../../lib/utils");
+const { TweetTypes } = require('../../../lib/constants');
 
 let userA;
 let userB;
@@ -22,16 +23,15 @@ describe("Given 2 authenticated users, userA & userB", () => {
             }
         })
 
-        describe("When userB replies to tweetA, ", () => {
-            const replyText = chance.string({ length: 16 })
-            let tweetReply;
+        describe("When userB replies to userA, tweetA, ", () => {
+            const replyTextA = chance.string({ length: 16 })
             beforeAll(async () => {
                 waitSec(1);
                 if (tweetA.id) {
-                    tweetReply = await when.invoke_reply({ 
-                        username: userB.username, 
-                        tweetId: tweetA.id, 
-                        text: replyText 
+                    await when.invoke_reply({
+                        username: userB.username,
+                        tweetId: tweetA.id,
+                        text: replyTextA
                     });
                 }
             })
@@ -39,11 +39,11 @@ describe("Given 2 authenticated users, userA & userB", () => {
             it("Saves the reply as new tweet", async () => {
                 const replyItems = await then.TweetsTable_replies_contains({
                     author: userB.username,
-                    inReplyToTweet: tweetA.id
+                    inReplyToTweetId: tweetA.id
                 })
 
                 const foundReply = replyItems.filter(r => {
-                    if (r.inReplyToTweet === tweetA.id && r.text === replyText) {
+                    if (r.inReplyToTweetId === tweetA.id && r.text === replyTextA) {
                         return r;
                     }
                 })[0]
@@ -51,17 +51,17 @@ describe("Given 2 authenticated users, userA & userB", () => {
                 expect(foundReply).toBeTruthy();
 
                 expect(foundReply).toMatchObject({
-                    __typename: "Reply",
-                    text: replyText,
+                    __typename: TweetTypes.REPLY,
+                    text: replyTextA,
                     author: userB.username,
                     replies: 0,
                     likes: 0,
                     retweets: 0,
-                    inReplyToTweet: tweetA.id,
-                    inReplyToUsers: [userA.username]
+                    inReplyToTweetId: tweetA.id,
+                    inReplyToUserIds: [userA.username]
                 })
             })
-            
+
             it("Increments Tweet.replies by 1", async () => {
                 const tweetItem = await then.TweetsTable_contains(tweetA.id)
                 expect(tweetItem.replies).toBe(1)
@@ -77,6 +77,66 @@ describe("Given 2 authenticated users, userA & userB", () => {
                 expect(timeline).toBeTruthy()
                 expect(timeline.length).toBe(1)
                 expect(timeline[0].inReplyToTweetId).toEqual(tweetA.id)
+            })
+        })
+        describe("When userB retweets to userA, tweetA, ", () => {
+            let userBRetweet;
+            beforeAll(async () => {
+                waitSec(1);
+                if (tweetA.id) {
+                    await when.invoke_retweet(userB.username, tweetA.id);
+
+                    waitSec(1);
+
+                    userBRetweet = await then.TweetsTable_retweets_contains({
+                        author: userB.username, 
+                        retweetOf: tweetA.id
+                    })
+                }
+            })
+
+            describe('When userA replies to userB retweet (userBRetweet)', () => {
+                const replyTextB = chance.string({ length: 16 })
+                
+                beforeAll(async () => {
+                    waitSec(1);
+                    if (userBRetweet.id) {
+                        await when.invoke_reply({
+                            username: userA.username,
+                            tweetId: userBRetweet.id,
+                            text: replyTextB
+                        });
+                    }
+                })
+
+                it("Saves the reply as new tweet", async () => {
+                    
+                    const replyItems = await then.TweetsTable_replies_contains({
+                        author: userA.username,
+                        inReplyToTweetId: userBRetweet.id
+                    })
+
+                    const foundReply = replyItems.filter(r => {
+                        if (r.inReplyToTweetId === userBRetweet.id && r.text === replyTextB) {
+                            return r;
+                        }
+                    })[0]
+
+                    expect(foundReply).toBeTruthy();
+
+                    expect(foundReply).toMatchObject({
+                        __typename: TweetTypes.REPLY,
+                        text: replyTextB,
+                        author: userA.username,
+                        replies: 0,
+                        likes: 0,
+                        retweets: 0,
+                        inReplyToTweetId: userBRetweet.id,
+                        inReplyToUserIds: expect.arrayContaining([userA.username, userB.username])
+                    })
+                    
+                    expect(foundReply.inReplyToUserIds).toHaveLength(2)
+                })
             })
         })
     })
