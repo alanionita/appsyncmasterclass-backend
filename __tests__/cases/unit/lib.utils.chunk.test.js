601fff92c9a2614ab25ab608afaf9d52f3f7c742
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-05-13 14:20:30 +0100

    FEAT (lib/util): adds chunk util and tests

---
+++ __tests__/cases/unit/lib.utils.chunk.test.js
@@ -0,0 +1,26 @@
+const { chunk } = require("../../../lib/utils");
+const chance = require('chance').Chance();
+
+describe("lib.util.chunk", () => {
+    it("Should create chunks from array", () => {
+        const input = [1, 2, 3, 4, 5, 6];
+        const chunkSize = 2;
+        const result = chunk(input, chunkSize)
+        expect(result.length).toEqual(3);
+        expect(result[0]).toStrictEqual([1, 2]);
+        expect(result[1]).toStrictEqual([3, 4]);
+        expect(result[2]).toStrictEqual([5, 6]);
+    })
+    it("Should create 25 item chunks from array", () => {
+        const input = Array(50).fill(null)
+        const numbers = input.map((n) => {
+            const cnat = chance.natural({min: 0, max: 200});
+            return cnat
+        })
+        const chunkSize = 25;
+        const result = chunk(numbers, chunkSize)
+        expect(result.length).toEqual(2);
+        expect(result[0]).toStrictEqual(numbers.slice(0, 25));
+        expect(result[1]).toStrictEqual(numbers.slice(-25));
+    })
+})
