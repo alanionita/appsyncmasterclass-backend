bed9017d85d641f870167a3cf608a5e770e77404
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-10-14 14:30:16 +0100

    FEAT (lambda/search): adds handler with support for searching users from algolia
    Implements the new .search() method from the userIndex
    
    Adds branching based on searchMode
    
    Adds support for nextToken
    
    Adds user.__typename to hits from Algolia

---
+++ functions/search.mjs
@@ -0,0 +1,135 @@
+import Chance from 'chance'
+import ssm from '@middy/ssm'
+import middy from '@middy/core'
+import { initUsersIndex } from "../lib/algolia.mjs"
+
+const { STAGE } = process.env;
+const chance = new Chance();
+
+const SearchMode = {
+    top: "Top",
+    latest: "Latest",
+    people: "People",
+    photos: "Photos",
+    video: "Videos"
+}
+
+function genNextToken(searchParams) {
+    if (!searchParams) {
+        return null
+    }
+
+    const payload = Object.assign(
+        {}, searchParams, { random: chance.string({ length: 16 }) })
+    const token = JSON.stringify(payload)
+    return Buffer.from(token).toString('base64')
+}
+
+function parseNextToken(nextToken) {
+    if (!nextToken) {
+        return undefined
+    }
+
+    const token = Buffer.from(nextToken, 'base64').toString()
+    const searchParams = JSON.parse(token)
+    delete searchParams.random
+
+    return searchParams
+}
+
+async function searchUsers({ context, userId, query, limit = 10, nextToken }) {
+    try {
+        const { ALGOLIA_APP_ID, ALGOLIA_WRITE_KEY } = context;
+
+        if (!ALGOLIA_APP_ID || !ALGOLIA_WRITE_KEY) {
+            throw Error("Missing context variables")
+        }
+
+        if (!userId || !query) {
+            throw Error("Missing required inputs")
+        }
+
+
+        const searchParams = parseNextToken(nextToken) || {
+            query,
+            hitsPerPage: limit,
+            page: 0
+        }
+
+        const usersIndex = await initUsersIndex({
+            appID: ALGOLIA_APP_ID,
+            apiKey: ALGOLIA_WRITE_KEY,
+            stage: STAGE
+        })
+
+        const results = await usersIndex.search(searchParams)
+
+        const { hits, page, nbPages } = results
+
+
+        hits.forEach(x => {
+            x.__typename = x.id === userId ? 'MyProfile' : 'OtherProfile'
+        })
+
+        let nextSearchParams
+        if (page + 1 >= nbPages) {
+            nextSearchParams = null
+        } else {
+            nextSearchParams = Object.assign({}, searchParams, { page: page + 1 })
+        }
+
+        return {
+            results: hits,
+            nextToken: genNextToken(nextSearchParams)
+        }
+    } catch (err) {
+        console.error('Err (searchLambda/searchUsers) :', err.message)
+        console.info('Err details) :', JSON.stringify(err))
+        return err
+    }
+}
+
+async function lambdaHandler(event, context) {
+    try {
+        if (!STAGE) {
+            throw Error("Missing environment variables")
+        }
+
+        const { query, mode, limit, nextToken } = event.arguments;
+
+        if (!query || !mode || !limit) {
+            throw Error("Missing required arguments")
+        }
+
+        const userId = event.identity.username
+
+        switch (mode) {
+            case SearchMode.people:
+                return await searchUsers({ context, userId, query, limit, nextToken })
+            case SearchMode.latest:
+                // TODO: add searchTweets
+                // return await searchTweets({ query, limit, nextToken })
+            default:
+                throw Error("Unsupported search mode")
+        }
+    } catch (err) {
+        console.error("Err [search] ::", err.message)
+        console.info(JSON.stringify(err.stack))
+        if (err.$metadata) {
+            console.info({ ...err.$metadata })
+        }
+    }
+}
+
+export const handler = middy()
+    .use(
+        ssm({
+            fetchData: {
+                ALGOLIA_APP_ID: `/${STAGE}/algolia-app-id`,
+                ALGOLIA_WRITE_KEY: `/${STAGE}/algolia-admin-key`
+            },
+            setToContext: true,
+            cacheExpiry: 5 * 60 * 1000, // 5 mins
+        })
+    )
+    .handler(lambdaHandler)
