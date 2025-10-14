951a912f9e9ea52a1cfe0a81dbb04cc2ec03137d
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-10-14 16:18:25 +0100

    FEAT (scripts/*): adds script to change the lastUpdate value on a Tweet
    Used to trigger downstream effects, namely algolia updates

---
+++ scripts/tweets-update-lastUpdated.mjs
@@ -0,0 +1,59 @@
+import AWS from 'aws-sdk'
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import * as path from 'path';
+import dotenv from 'dotenv'
+
+const __dirname = import.meta.dirname;
+
+dotenv.config({ path: path.resolve(__dirname, '../.env'), })
+
+AWS.config.region = process.env.REGION
+
+const run = async () => {
+    const loop = async (exclusiveStartKey) => {
+        try {
+
+            const ddb = new DynamoDBClient({ region: process.env.REGION });
+            const client = new DynamoDBDocumentClient(ddb)
+
+            const input = {
+                TableName: process.env.TWEETS_TABLE,
+                ExclusiveStartKey: exclusiveStartKey,
+                Limit: 100
+            };
+
+            const command = new ScanCommand(input);
+            const ddbResp = await client.send(command)
+
+            if (ddbResp.$metadata.httpStatusCode === 200) {
+                const updatePromises = ddbResp.Items.map(async x => {
+                    const updateInput = {
+                        TableName: process.env.TWEETS_TABLE,
+                        Key: {
+                            id: x.id
+                        },
+                        UpdateExpression: "SET lastUpdated = :now",
+                        ExpressionAttributeValues: {
+                            ":now": new Date().toJSON()
+                        }
+                    }
+
+                    const command = new UpdateCommand(updateInput)
+                    await client.send(command)
+                })
+                await Promise.all(updatePromises)
+                if (ddbResp.LastEvaluatedKey) {
+                    return await loop(ddbResp.LastEvaluatedKey)
+                }
+            }
+        } catch (err) {
+            console.error('Err [scripts/tweets-update-lastUpdated] : ', err.message)
+            console.info(JSON.stringify(err))
+        }
+    }
+
+    await loop()
+}
+
+run().then(x => console.info('INFO [scripts/tweets-update-lastUpdated] : DONE'))
