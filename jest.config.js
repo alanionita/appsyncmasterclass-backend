284215e289ec2295b30e2e0362eabba2fd734879
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-10-29 10:08:25 +0000

    FEAT (packages): add babel and babe-jest
    Configures jest to handle ESM modules

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     testEnvironment: 'node',
-    testMatch: ['**/__tests__/cases/**/*']
+    testMatch: ['**/__tests__/cases/**/*'],
+    transform: {
+        '^.+\\.mjs$': 'babel-jest',
+    },
+    moduleFileExtensions: ['js', 'mjs', 'cjs', 'json'],
+
 }
