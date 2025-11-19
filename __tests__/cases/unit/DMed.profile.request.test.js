2b5d1cb4414156dcf2d0063b09eae7ce9346f14c
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-11-19 20:19:50 +0000

    FEAT (appsync): adds DMed.profile nested resolver templates
    Adds unit test to check logic

---
+++ __tests__/cases/unit/DMed.profile.request.test.js
@@ -0,0 +1,32 @@
+const given = require("../../steps/given")
+const when = require("../../steps/when")
+const chance = require("chance").Chance();
+const path = require("path");
+
+describe("DMed.profile.request template", () => {
+    it("Should return valid query", () => {
+        const templatePath = path.resolve(__dirname, "../../../appsync/resolvers/DMed.profile.request.vtl")
+        const username = chance.guid()
+
+        const context = given.random_appsync_contextV2({
+            identity: { username },
+            args: {},
+            result: {},
+            source: {
+                otherUserId: username
+            }
+        })
+
+        const result = when.invoke_appsync_template(templatePath, context);
+
+        expect(result).toEqual({
+            "operation": "GetItem",
+            "version": "2018-05-29",
+            "key": {
+                "id": {
+                    "S": username
+                }
+            }
+        })
+    })
+})
